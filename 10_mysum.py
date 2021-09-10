ab975f2733383488e2fa3ab2c39ad92f60568621
Andy Wang <climber2002@gmail.com>
2021-09-10 10:55:39 +1000

    ex10

---
+++ 10_mysum.py
@@ -0,0 +1,9 @@
+def mysum(*items):
+    if not items:
+        return items
+    output = items[0]
+    for item in items[1:]:
+        output += item
+    return output
+
+print(mysum(10, 20, 30, 40))
